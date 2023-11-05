6367cf00b6cc36cf1c6912224a581d40e4df872a
amv0107 <amv0107@gmail.com>
2023-11-05 23:05:07 +0200

    decision

--- app/src/main/java/com/amv0107/workwithpermission/MainActivity.kt
+++ app/src/main/java/com/amv0107/workwithpermission/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.amv0107.workwithpermission
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(RequestPermission()){
+        if (it){
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test 1
+
+        requestPermission()
+    }
+
+    private fun requestPermission(){
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
